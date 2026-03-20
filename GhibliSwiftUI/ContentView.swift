b032d1071e8cedc9f1e168c6d4fe90a74b13701b
Xiao Yuan Lv <xiaoyuan.drhorse@gmail.com>
2026-03-20 13:25:10 +0630

    Initial Commit

---
+++ GhibliSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GhibliSwiftUI
+//
+//  Created by Xiao Yuan Lv on 3/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GhibliSwiftUI/GhibliSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliSwiftUIApp.swift
+//  GhibliSwiftUI
+//
+//  Created by Xiao Yuan Lv on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GhibliSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
